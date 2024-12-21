a20a2e8e517a528b77afac2c07e5f9127d6a6eb9
Vasco Ribeiro <vascogribeiro@gmail.com>
2024-12-21 14:41:59 +0000

    funções obter vários, adicionar e actualizar na api actualizadas

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const employeeModel = mongoose.model("Employees", employeeSchema);
 
 mongoose.connect(mongooseQueryString);
 
+//função obter todos Employees
 async function queryAllEmployee(){
 
     try{
         let queryAllEmployeeResults = await employeeModel.find();
-        console.log(queryAllEmployeeResults);
         return queryAllEmployeeResults;
     }
     catch(error){
@@ -22,19 +22,70 @@ async function queryAllEmployee(){
     }
 }
 
-/*app.use((request,response,next) => {
-    console.log(`Foi efetuado um pedido de ${request.method} ao url ${request.url}`);
-    next();
-}) */
-
 app.use(express.json());
 
 app.get("/Employees", async (request,response) => {
     let result = await queryAllEmployee();
-        console.log(result);
         response.status(200).json(result);
     }
 )
 
+//função obter apenas um Employee
+async function queryOneEmployee(params){
+
+    try{
+        let queryOneEmployeeResults = await employeeModel.findById(params);
+        return queryOneEmployeeResults;
+    }
+    catch(error){
+        console.log(error);
+        return [];
+    }
+} 
+
+app.get("/Employees/:id", async (request,response) => {
+    let resultOne = await queryOneEmployee(request.params.id);
+    response.send(resultOne)
+})
+
+//função adicionar Employee
+async function addEmployee(body){
+    let Employee = new employeeModel({
+        Name: body.Name,
+        EmpID: body.EmpID,
+        Age: body.Age,
+        Address: body.Address,
+    });
+    try{
+        await Employee.save();
+    }
+    catch(error){
+        return "error";
+    }
+}
+
+app.post("/Employees", async function (request,response) {
+    let result = await addEmployee(request.body);
+    response.json(result)
+})
+
+//função update Employee
+app.put("/Employees/:id", async function (request,response){
+    try{
+        const{id} = request.params;
+
+        const Employee = await employeeModel.findByIdAndUpdate(id, request.body);
+
+        if(!Employee) {
+            return response.status(404).json({message: "Employee Not Found"});
+        }
+
+        const updatedEmployee = await employeeModel.findById(id);
+        response.status(200).json(updatedEmployee)
+    }
+    catch(error){
+        response.status(500).json({message: error.message});
+    }
+});
 
 app.listen(appPort, () =>{console.log("Running on 127.0.0.1:"+appPort)});
